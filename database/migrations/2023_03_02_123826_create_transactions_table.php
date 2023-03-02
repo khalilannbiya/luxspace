86ff9a4634fce36bf8b2271a062a4d28e79ae192
khalilannbiya <syeichkhalil@gmail.com>
2023-03-02 19:45:43 +0700

    feat: create Model Transaction & migration create transactions table

---
+++ database/migrations/2023_03_02_123826_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('users_id');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->text('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('courier')->nullable();
+            $table->string('payment')->default('MIDTRANS');
+            $table->string('payment_url')->nullable();
+            $table->bigInteger('total_price')->default(0);
+            $table->string('status')->default('PENDING');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
