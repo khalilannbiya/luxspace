7843a2e0cb2af20b9e6a3632fb06c83f07bda45d
khalilannbiya <syeichkhalil@gmail.com>
2023-03-05 19:27:17 +0700

    feat: create DashboardController, change Route, fix error when Route is changed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,8 +27,8 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+])->name('dashboard.')->prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+    Route::middleware(['admin'])->group(function () {
+    });
 });
