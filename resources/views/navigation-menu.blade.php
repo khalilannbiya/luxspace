098708c6856ef13287a1898447daa3d5d0d7de1d
khalilannbiya <syeichkhalil@gmail.com>
2023-03-09 07:52:02 +0700

    feat: change nav link

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,10 +15,12 @@
                     <x-nav-link href="{{ route('dashboard.index') }}" :active="request()->routeIs('dashboard.index')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @if (Auth::user()->roles == 'ADMIN')
                     <x-nav-link href="{{ route('dashboard.product.index') }}"
-                        :active="request()->routeIs('dashboard.product.index') || request()->routeIs('dashboard.product.create')">
-                        {{ __('Product') }}
+                        :active="request()->routeIs('dashboard.product.index')">
+                        {{ __('Products') }}
                     </x-nav-link>
+                    @endif
                 </div>
             </div>
 
@@ -152,9 +154,16 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard.index') }}" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link href="{{ route('dashboard.index') }}"
+                :active="request()->routeIs('dashboard.index')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            @if (Auth::user()->roles == 'ADMIN')
+            <x-responsive-nav-link href="{{ route('dashboard.product.index') }}"
+                :active="request()->routeIs('dashboard.product.index')">
+                {{ __('Products') }}
+            </x-responsive-nav-link>
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
